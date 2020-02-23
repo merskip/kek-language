37b3cd5489ec9d0c6c7157075a64730b744847f4
Piotr Merski <merskip@gmail.com>
2020-02-23 10:33:06 +0100

    More coloring LLVM IR

--- src/main/kotlin/pl/merskip/keklang/StringLLVMIR.kt
+++ src/main/kotlin/pl/merskip/keklang/StringLLVMIR.kt
@@ -1,8 +1,12 @@
 package pl.merskip.keklang
 
 fun String.colorizeLLVMIR() =
-    colorize("\\%[a-zA-Z0-9._]+".toRegex(), Color.Blue)
-        .colorize("\\@[a-zA-Z0-9._]+".toRegex(), Color.Cyan)
+    this
+        .colorize("%[a-zA-Z0-9._]+".toRegex(), Color.Blue)
+        .colorize(" [0-9]+".toRegex(), Color.Blue)
+        .colorize("i[0-9]+".toRegex(), Color.Green)
+        .colorize("void", color = Color.Green)
+        .colorize("@[a-zA-Z0-9._]+".toRegex(), Color.Cyan)
         .colorize(";.*?\n".toRegex(), Color.BrightBlack)
         .colorize(".*?:\n".toRegex(), Color.Magenta)
 
@@ -10,4 +14,4 @@ fun String.colorize(regex: Regex, color: Color): String =
     replace(regex, "$0".colored(color))
 
 fun String.colorize(text: String, color: Color): String =
-    replace(text, "$0".colored(color))
+    replace(text, text.colored(color))
