8fee77d77990eb3f3aa03e5e10d4f53ae8f307bb
Piotr Merski <merskip@gmail.com>
2020-12-27 07:54:20 +0100

    Parsing data layout

--- src/main/kotlin/pl/merskip/keklang/llvm/RicherLLVMIRText.kt
+++ src/main/kotlin/pl/merskip/keklang/llvm/RicherLLVMIRText.kt
@@ -24,6 +24,7 @@ class RicherLLVMIRText(
     private val typeIntegerRegex = Regex("i[0-9]+\\**")
     private val typeFloatingPointRegex = Regex("(half|bfloat|float|double|fp128|x86_fp80|ppc_fp128)\\**")
     private val typeVoidRegex = Regex("void")
+
     // TODO: Add vector type, array type, structure type, improve pointer type
     private val anyTypeRegex = oneOf(typeIntegerRegex, typeFloatingPointRegex, typeVoidRegex)
 
@@ -45,15 +46,120 @@ class RicherLLVMIRText(
     /** See [https://llvm.org/docs/LangRef.html#attribute-groups] */
     private val attributeRegex = Regex("#[0-9]+")
 
-    private val stringRegex = Regex("\".*?\"")
+    private val stringRegex = Regex("\"(.*?)\"")
     private val labelRegex = Regex(".*:")
     private val terminatorInstructionRegex = Regex("ret|br|switch|indirectbr|invoke|callbr|resume|catchswitch|catchret|cleanupret|unreachable")
 
     fun rich(): String {
         val lines = plainLLVMIR.lineSequence().toMutableList()
+        parseDataLayout(lines)
         recognizeMangledIdentifiers(lines)
         highlightSyntax(lines)
         return lines.joinToString(System.lineSeparator())
+    }
+
+    private fun parseDataLayout(lines: MutableList<String>) {
+        val prefix = "target datalayout = "
+        val lineIterator = lines.listIterator()
+        for (line in lineIterator) {
+            if (line.startsWith(prefix)) {
+                val dataLayout = stringRegex.find(line)?.groupValues?.get(1)
+                if (dataLayout != null) {
+                    logger.verbose("Found data layout: \"$dataLayout\"")
+                    val specifications = dataLayout.split('-')
+                    for (specification in specifications) {
+                        val description = getDataLayoutSpecificationDescription(specification)
+                        lineIterator.addBefore("; $specification -> ${description ?: "Unknown"}")
+
+                        if (description == null)
+                            logger.warning("Unknown data layout specification of \"$specification\"")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getDataLayoutSpecificationDescription(string: String): String? {
+        /** See [https://llvm.org/docs/LangRef.html#data-layout] */
+        return when {
+            string == "E" -> "Big-Endian"
+            string == "e" -> "Little-Endian"
+            string.startsWith("S") -> "Stack alignment is ${string.skipPrefix('S')} bits"
+            string.startsWith("P") -> "Program address space is ${string.skipPrefix('P')}"
+            string.startsWith("G") -> "Global value address space is ${string.skipPrefix('G')}"
+            string.startsWith("A") -> "Address space for 'alloca' is ${string.skipPrefix('A')}"
+            string.startsWith("p") -> {
+                val chunks = string.split(':')
+                val addressSpace = chunks[0].skipPrefix('p').ifEmpty { "0" }
+                val size = chunks.getOrNull(1) ?: "unspecified"
+                val abi = chunks.getOrNull(2) ?: "unspecified"
+                val alignment = chunks.getOrNull(3) ?: "unspecified"
+                val idx = chunks.getOrNull(4) ?: "unspecified"
+                "For address space $addressSpace pointer size is $size bits, ABI $abi, alignment is $alignment bits, size of index is $idx"
+            }
+            string.startsWith("i") -> {
+                val (size, abi, alignment) = getSizeABIAndAlignment(string, "i")
+                "Alignment of integer type of size $size bits and ABI $abi is $alignment bits"
+            }
+            string.startsWith("v") -> {
+                val (size, abi, alignment) = getSizeABIAndAlignment(string, "v")
+                "Alignment of vector type of size $size bits and ABI $abi is $alignment bits"
+            }
+            string.startsWith("f") -> {
+                val (size, abi, alignment) = getSizeABIAndAlignment(string, "f")
+                "Alignment of floating-point type of size $size bits and ABI $abi is $alignment bits"
+            }
+            string.startsWith("a") -> {
+                val chunks = string.split(':')
+                var abi = chunks.getOrNull(1) ?: "unspecified"
+                val alignment = chunks.getOrNull(2) ?: run {
+                    abi = "unspecified"
+                    chunks.getOrNull(1) ?: "unspecified"
+                }
+                "Alignment of object of aggregate and ABI $abi is $alignment bits"
+            }
+            string.startsWith("F") -> {
+                val type = string[1]
+                val abi = string.substring(2)
+                "Alignment of function pointers is " + when (type) {
+                    'i' -> "independent of the alignment of functions, and is a multiple of $abi"
+                    'n' -> "a multiple of the explicit alignment specified on the function, and is a multiple of $abi"
+                    else -> "$type <unknown>"
+                }
+            }
+            string.startsWith("m") -> {
+                val mangling = string.skipPrefix("m:")
+                "Mangling names is " + when (mangling) {
+                    "e" -> "ELF"
+                    "m" -> "Mips"
+                    "o" -> "Mach-O"
+                    "x" -> "Windows x86 COFF"
+                    "w" -> "Windows COFF"
+                    "a" -> "XCOFF"
+                    else -> "$mangling <unknown>"
+                }
+            }
+            string.startsWith("ni") -> {
+                val addressSpaces = string.skipPrefix("ni:").split(":")
+                "Address spaces for non-integral pointer types are ${addressSpaces.joinToString()}"
+            }
+            string.startsWith("n") -> {
+                val sizes = string.skipPrefix('n').split(":")
+                "Native integers widths of CPU are ${sizes.joinToString()}"
+            }
+            else -> null
+        }
+    }
+
+    private fun getSizeABIAndAlignment(string: String, prefix: String): Triple<String, String, String> {
+        val chunks = string.split(':')
+        val size = chunks[0].skipPrefix(prefix)
+        var abi = chunks.getOrNull(1) ?: "unspecified"
+        val alignment = chunks.getOrNull(2) ?: run {
+            abi = "unspecified"
+            chunks.getOrNull(1) ?: "unspecified"
+        }
+        return Triple(size, abi, alignment)
     }
 
     private fun recognizeMangledIdentifiers(lines: MutableList<String>) {
@@ -64,9 +170,7 @@ class RicherLLVMIRText(
                 val type = typesRegister.findTypeOrNull(matchIdentifier.value.substring(1))
                 if (type != null) {
                     logger.verbose("Matched \"${matchIdentifier.value}\" to \"${type.getDebugDescription()}\"")
-                    lineIterator.previous()
-                    lineIterator.add("; " + type.getDebugDescription())
-                    lineIterator.next()
+                    lineIterator.addBefore("; " + type.getDebugDescription())
                 }
             }
         }
@@ -129,4 +233,16 @@ class RicherLLVMIRText(
         return isInsideString
     }
 
+    private fun String.skipPrefix(prefix: Char) = skipPrefix(prefix.toString())
+
+    private fun String.skipPrefix(prefix: String): String {
+        return if (startsWith(prefix)) substring(prefix.length)
+        else error("The string doesn't starts with $prefix")
+    }
+
+    private fun <T> MutableListIterator<T>.addBefore(element: T) {
+        previous()
+        add(element)
+        next()
+    }
 }
