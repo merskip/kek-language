00b5019a2d883fdd8d154ea9c4e57684c5e3bcf1
Piotr Merski <merskip@gmail.com>
2021-01-15 08:27:11 +0100

    Migrating to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+}
+
+group = "pl.merskip"
+version = "0.1"
+
+apply(plugin = "kotlin")
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.61")
+    implementation(group = "org.bytedeco", name = "llvm-platform", version = "10.0.1-1.5.4")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.3.61")
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
+}
+
+tasks.compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.compileTestKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
